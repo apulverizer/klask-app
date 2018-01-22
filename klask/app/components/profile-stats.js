7d77e5e5c6a21c8b3480739a01f51fb45c4581a3
Aaron Pulver <apulver@esri.com>
2018-01-21 21:28:24 -0500

    cleanup extra folder

--- klask/app/components/profile-stats.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  wins: null,
-  losses: null,
-  winPercentage: null,
-  goalsFor: null,
-  goalsAgainst: null
-});
--- klask/app/components/standings-list.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-import { inject } from '@ember/service';
-
-export default Component.extend({
-  tagName: 'ul',
-  classNames: ['demo-list-two mdl-list'],
-  store: inject(),
-  
-});
