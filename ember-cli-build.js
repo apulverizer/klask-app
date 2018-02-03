2bc370cbe640a1efbfed8d52e6066bea317fbfd1
Dawit Elias <dawitelias14@gmail.com>
2018-02-03 16:35:34 -0500

    Using ember-paper instead of material css + js library
    NOTE: haven't tweaked the design (will do later). Wanted to use ember-paper's components to get the arenas screen working properly.
    
    Both the text field and previously joined arenas list are working. Next will use ember-paper components for the toolbar and profile overflow menu

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,6 +16,7 @@ module.exports = function(defaults) {
     }
   });
 
+  // app.import('vendor/material-min.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
