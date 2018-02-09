22d39a6724453396208c9d94c345db4ce7e590a8
Aaron Pulver <apulver@esri.com>
2018-02-08 21:19:05 -0500

    wip

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,33 @@
 import Controller from '@ember/controller';
+import { inject } from '@ember/service';
 
 export default Controller.extend({
+  store: inject(),
   actions: {
     signIn: function(provider) {
-      var parent_this = this;
+      var self = this;
       this.get('session').open('firebase', { provider: provider}).then(function(data) {
         // do something with data.currentUser
-        parent_this.transitionToRoute('arenas');
+        let users = self.get('store').query('user', {
+          orderBy: 'uid',
+          equalTo: data.currentUser.uid
+        }).then(function(users){
+          if (users.get('length') < 1){
+              let newUser = self.get('store').createRecord('user', {
+                arenasjoined: null,
+                nickname: null,
+                email: data.currentUser.email,
+                name: data.currentUser.displayName,
+                photourl: data.currentUser.photoURL,
+                uid: data.currentUser.uid
+              });
+              newUser.save().then(function(){
+                self.transitionToRoute('arenas');
+              }).catch(function(error){
+                console.log(error);
+              })
+          }
+        });
       });
     }
   }
