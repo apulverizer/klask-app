85ab3342f25c4c75391b92b4f20418a24f49d337
Aaron Pulver <apulver@esri.com>
2018-02-11 20:09:33 -0500

    refactor

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,36 +3,37 @@ import { inject } from '@ember/service';
 
 export default Controller.extend({
   store: inject(),
+  session: inject(),
   actions: {
     signIn: function(provider) {
       var self = this;
       this.get('session').open('firebase', { provider: provider}).then(function(data) {
-        // do something with data.currentUser
-        let users = self.get('store').query('user', {
+        // query for the user by id
+        return self.get('store').query('user', {
           orderBy: 'uid',
           equalTo: data.currentUser.uid
-        }).then(function(users){
-          // check if user already exists
-          // if not, then create a new user
-          if (users.get('length') < 1){
-              let newUser = self.get('store').createRecord('user', {
-                arenasjoined: null,
-                nickname: null,
-                email: data.currentUser.email,
-                name: data.currentUser.displayName,
-                photourl: data.currentUser.photoURL,
-                uid: data.currentUser.uid
-              });
-              newUser.save().then(function(){
-                self.transitionToRoute('arenas');
-              }).catch(function(error){
-                console.log(error);
-              })
-          }
-          else {
-            self.transitionToRoute('arenas');
-          }
-        });
+        })
+      })
+      .then(function(users){
+        // check if user already exists
+        // if not, then create a new user
+        if (users.get('length') < 1){
+          let newUser = self.get('store').createRecord('user', {
+            arenasjoined: null,
+            nickname: null,
+            email: self.get('session.currentUser.email'),
+            name: self.get('session.currentUser.displayName'),
+            photourl: self.get('session.currentUser.photoURL'),
+            uid: self.get('session.currentUser.uid')
+          });
+          return newUser.save();
+        }
+        // no need to add user, return resolved promise
+        return Promise.resolve();
+      })
+      .then(function(){
+        // route to arenas screen after sign in
+        self.transitionToRoute('arenas');
       });
     }
   }
