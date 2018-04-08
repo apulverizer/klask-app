8b4cffadeea939aabe43a40767b5a99c8835e045
Aaron Pulver <apulver@esri.com>
2018-04-07 20:20:49 -0400

    add ability to record game scores remove FAB

--- app/components/score-card.js
+++ app/components/score-card.js
@@ -1,12 +1,37 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
-    user: null,
+    opponent: null,
     win: null,
     score: 0,
+    store: inject(),
+    session: inject(),
     actions: {
         submitScore(){
-            console.log(this.get('score'));
+            let player1score = 0;
+            let player2score = 0;
+            if (this.get('win')){
+                player1score = 6;
+                player2score = this.get('score');
+            }
+            else {
+                player2score = 6;
+                player1score = this.get('score');
+            }
+            let newGame = this.get('store').createRecord('game',{
+                arenaid: localStorage.getItem('arenaId'),
+                datetime: new Date().getTime()/1000,
+                player1id: this.get('session.currentUser.uid'),
+                player1score: player1score,
+                player2id: this.get('opponent.uid'),
+                player2score: player2score,
+                gid: null,
+            })
+            newGame.set('gid', newGame.get('id'));
+            newGame.save().catch(function(e){
+                console.log(e)
+            })
         },
         score0(){
             this.set('score', 0);
