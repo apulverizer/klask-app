3c031b79e2d3d2e99943dd18be9f8660dca809b0
Aaron Pulver <apulverizer@gmail.com>
2018-04-08 15:56:44 -0400

    optional header buttons (#31)
    * optional header buttons
    
    * changes to show 2 types of profile menus
    
    * add back button option

--- app/components/klask-header.js
+++ app/components/klask-header.js
@@ -6,6 +6,14 @@ export default Component.extend({
   session: inject(),
   title: null,
   username: null,
+  showBackButton: false,
+  // profile buttons are mutually exclusive
+  showMyProfileButton: false,
+  showOtherProfileButton: computed('showMyProfileButton', function(){
+    return !this.get('showMyProfileButton');
+  }),
+  showEditButton: false,
+  showSwapButton: false,
   userid: computed('session', function(){
     return this.get('session.currentUser.uid');
   }),
